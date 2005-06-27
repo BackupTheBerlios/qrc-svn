e21ed829431734b7ea951d1e4c15de2fedcb1af4
deckrider <deckrider@f1d58633-e4f7-0310-83e4-944453b9de29>
2005-06-27 13:49:20 +0000

    Fixed a rather ugly problem that caused unknown messages to be sent to the server for every buddy on the list, if one buddy was added that contained a newline.  Cutting and pasting would sometimes cause this to happen if the user wasn't careful.

--- trunk/gaym/src/gaym.c
+++ trunk/gaym/src/gaym.c
@@ -607,6 +607,9 @@ static void gaym_set_away(GaimConnection * gc, const char *state,
 static void gaym_add_buddy(GaimConnection * gc, GaimBuddy * buddy,
                            GaimGroup * group)
 {
+    buddy->name = g_strstrip(buddy->name);
+    buddy->alias = g_strstrip(buddy->alias);
+    buddy->server_alias = g_strstrip(buddy->server_alias);
     struct gaym_conn *gaym = (struct gaym_conn *) gc->proto_data;
     struct gaym_buddy *ib = g_new0(struct gaym_buddy, 1);
     ib->name = g_strdup(buddy->name);
