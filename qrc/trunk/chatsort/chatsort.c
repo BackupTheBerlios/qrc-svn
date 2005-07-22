62bc46868b47b7dc0010b0c5690c87685b2007e9
jblebrun <jblebrun@f1d58633-e4f7-0310-83e4-944453b9de29>
2005-07-22 04:22:19 +0000

    Added chaticon option to the configure script. Cleaned up chatsort code.

--- qrc/trunk/chatsort/chatsort.c
+++ qrc/trunk/chatsort/chatsort.c
@@ -1,6 +1,4 @@
-/* Puts last 4k of log in new conversations a la Everybuddy (and then
- * stolen by Trillian "Pro") */
-
+/* Attempt to sort chat users by entry order, instead of alpha */
 #include "internal.h"
 #include "gtkgaim.h"
 
@@ -17,10 +15,9 @@
 #include "gtkplugin.h"
 
 #define CHATSORT_PLUGIN_ID "gtk-chatsort"
-#define CHATSORT_USERS_COLUMNS 4
-#define CHATSORT_USERS_ENTRY_COLUMN 4
 
-gint entry_id=0;
+
+//A dummy sort function... don't sort at all!
 static gint sort_chat_users(GtkTreeModel *model, GtkTreeIter *a, GtkTreeIter *b, gpointer userdata) {
 return 1;
 }
@@ -28,68 +25,24 @@ return 1;
 //This gets called BEFORE a chatlist is populated... just creates a new type of chat window.
 static void redochatwindow(GaimConversation *c) {
 
-	GtkListStore *ls;
-	/**
-	 * Unused variables:
-	 *
-	 * GtkCellRenderer *rend;
-	 * GtkTreeViewColumn *col;
-	 */
 	GtkTreeModel *oldls;
+	GtkTreeSelection *select;
 	GtkTreeIter iter;
-	
+
 	//Get a handle to the chat pane for the conversation
 	GaimGtkConversation* gtkconv = GAIM_GTK_CONVERSATION(c);
 	GaimGtkChatPane* gtkchat = gtkconv->u.chat;
 
 	
-	//Remember the list in case the plugin is unloaded.
 	oldls = gtk_tree_view_get_model(GTK_TREE_VIEW(gtkchat->list));
+	select = gtk_tree_view_get_selection(GTK_TREE_VIEW(gtkchat->list));
 
-	//Make a new tree list
-
-	//Columns: Icon, Name, Flags, Entry order
-	ls = gtk_list_store_new(CHATSORT_USERS_COLUMNS, GDK_TYPE_PIXBUF, G_TYPE_STRING,
-                                                        G_TYPE_INT, G_TYPE_INT);
-	gtk_list_store_append(ls, &iter);
-        gtk_list_store_set(ls, &iter, CHAT_USERS_NAME_COLUMN, "ChatSort", CHATSORT_USERS_ENTRY_COLUMN, 1, -1);
-	//Eventually, should have an entry for default.
-        gtk_tree_sortable_set_sort_func(GTK_TREE_SORTABLE(ls), CHAT_USERS_NAME_COLUMN, sort_chat_users, NULL, NULL);
-	
-	gtk_tree_sortable_set_sort_column_id(GTK_TREE_SORTABLE(ls), CHAT_USERS_NAME_COLUMN,
-                                                                               GTK_SORT_DESCENDING);
-
-        gtk_tree_view_set_model(GTK_TREE_VIEW(gtkchat->list), GTK_TREE_MODEL(ls));
-
-	//rend = gtk_cell_renderer_pixbuf_new();
-        //col = gtk_tree_view_column_new_with_attributes(NULL, rend, "pixbuf", CHAT_USERS_ICON_COLUMN, NULL);
-        //gtk_tree_view_column_set_clickable(GTK_TREE_VIEW_COLUMN(col), TRUE);
-        //gtk_tree_view_append_column(GTK_TREE_VIEW(list), col);
-
-	//Maybe eventually make our own menu.
-	//g_signal_connect(G_OBJECT(list), "button_press_event",
-        //                                 G_CALLBACK(right_click_chat_cb), conv);
-        //g_signal_connect(G_OBJECT(list), "popup-menu",
-        //                 G_CALLBACK(gtkconv_chat_popup_menu_cb), conv);
-
-        //rend = gtk_cell_renderer_text_new();
-        //col = gtk_tree_view_column_new_with_attributes(NULL, rend, "text", CHAT_USERS_NAME_COLUMN, NULL); 
-        //gtk_tree_view_column_set_clickable(GTK_TREE_VIEW_COLUMN(col), TRUE);
-        //gtk_tree_view_append_column(GTK_TREE_VIEW(list), col);
-       
-       //gtk_widget_set_size_request(list, 150, -1);
-        //gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(list), FALSE);
-
-	gaim_debug_misc("chatsort","redo window\n");
-
-	
-
-}
-
-static void joined(GaimConversation *c, const char* name, GaimConvChatBuddyFlags flags) {
-
-	gaim_debug_misc("chatsort","Joined: %s\n",name);
-	gaim_debug_misc("chatsort","Flags: %x\n",flags);
+	//This is a dummy "root" item. If it's not here,
+	//then the first name entered into the list gets "stucK" at the
+	//top. This is a hack.
+	gtk_list_store_append(GTK_LIST_STORE(oldls), &iter);
+	gtk_list_store_set(GTK_LIST_STORE(oldls), &iter, CHAT_USERS_NAME_COLUMN, " ", -1);
+        gtk_tree_sortable_set_sort_func(GTK_TREE_SORTABLE(oldls), CHAT_USERS_NAME_COLUMN, sort_chat_users, NULL, NULL);
 	
 }
 
@@ -99,11 +52,6 @@ static gboolean plugin_load(GaimPlugin *plugin)
 						"chat-joined",
 						plugin, GAIM_CALLBACK(redochatwindow), NULL);
 	
-	gaim_signal_connect(gaim_conversations_get_handle(),
-						"chat-buddy-joined",
-						plugin, GAIM_CALLBACK(joined), NULL);
-	
-
 	return TRUE;
 }
 
